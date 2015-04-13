90337a4d74ee4cd47fce45fa0d1bc76239186706
Omri Mendels <omri.mendels@intel.com>
2015-04-14 00:36:40 +0300

    more tests

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,10 @@
 ## This program allows the caching of the calculation of a matrix inverse. Instead of re-calculating the inverse,
 # one can use the cached value in case it was already calculated.
 
-#makeCacheMatrix returns a list of functions. Each function returns:
-# get: returns the original matrix
-# set: sets the original matrix
-# setinverse: sets the inverse of the input matrix
+#makeCacheMatrix holds a matrix and returns a list of functions. Each function returns:
+# get(): returns the original matrix
+# set(matrix): sets the original matrix
+# setinverse(inv.matrix): sets the inverse of the input matrix
 # getinverse: gets the inverse of the input matrix if it was already calculated. otherwise it returns NULL.
 makeCacheMatrix <- function(x = matrix()) {
   m <- NULL
@@ -37,25 +37,43 @@ cacheSolve <- function(x, ...) {
 }
 
 
-##TESTING
+#### TESTING ####
+
+#input
 v1<-c(0,0,0,0,1)
 v2<-c(1,0,0,0,1)
 v3<-c(1,1,0,0,1)
 v4<-c(1,1,1,0,1)
 v5<-c(1,1,1,1,1)
 mat<-rbind(v1,v2,v3,v4,v5)
-class(mat) #should be "matrix"
 
-#create the special matrix:
-special.mat<-makeCacheMatrix(mat)
-#1. retrieve original matrix
-mat2<-special.mat$get()
-mat==mat2
+class(mat) #class is "matrix"
 
-#2. set new matrix
+#create the cached matrix:
+cached.mat<-makeCacheMatrix(mat)
+#1. retrieve original matrix using get()
+mat2<-cached.mat$get()
+mat==mat2 #equals
+
+#2. set new matrix using set(matrix)
 mat[5,5]<-5
-special.mat$set(mat)
-special.mat2<-special.mat$get()
-mat==special.mat2
+cached.mat$set(mat)
+cached.mat2<-cached.mat$get()
+mat==cached.mat2
 
-#3. calculate inverse for the first time
+#3. calculate inverse for the first time with cacheSolve
+inverse<-cacheSolve(cached.mat)
+#see that it really is the inverse (result equals I)
+(inverse%*%mat)
+
+#4. calculate inverse again (returns cached inverse, see message)
+inverse2<-cacheSolve(cached.mat)
+inverse2==inverse
+
+#5. change the matrix, re run solve (no message given)
+mat[5,5]<-15
+mat3<-cached.mat$set(mat)
+inverse3<-cacheSolve(cached.mat)
+
+#6. re run solve (message given, cached value returned)
+inverse4<-cacheSolve(cached.mat)
